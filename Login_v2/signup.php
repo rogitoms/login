c40ae4b58140f7f1edd1e1701397ad6e34605340
rogitoms <marilynn.rogito@strathmore.edu>
2024-10-02 23:54:57 +0300

    ........ ,,

--- Login_v2/signup.php
+++ Login_v2/signup.php
@@ -5,16 +5,16 @@ include 'db.php';  // Ensure you have db.php file with the database connection
 // Check if the form is submitted
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     // Retrieve the form data
-    $username = $_POST['username'];
+    $email = $_POST['email'];
     $password = $_POST['password'];
 
     // Hash the password before storing it
     $hashed_password = password_hash($password, PASSWORD_DEFAULT);
 
     // Prepare an SQL query to insert the user into the database
-    $query = "INSERT INTO users (username, password) VALUES (?, ?)";
+    $query = "INSERT INTO users (email, password) VALUES (?, ?)";
     $stmt = $conn->prepare($query);
-    $stmt->bind_param("ss", $username, $hashed_password);  // 'ss' specifies both parameters are strings
+    $stmt->bind_param("ss", $email, $hashed_password);  // 'ss' specifies both parameters are strings
 
     // Execute the query
     if ($stmt->execute()) {
